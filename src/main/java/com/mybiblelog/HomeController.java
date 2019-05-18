bdab782955f40c70e062d203c22ae91b5438d25d
Aaron <aaronsmith.xci@gmail.com>
2019-05-17 23:32:25 -0400

    Add Thymeleaf layout dialect

--- src/main/java/com/mybiblelog/HomeController.java
+++ src/main/java/com/mybiblelog/HomeController.java
@@ -1,12 +1,19 @@
 package com.mybiblelog;
 
+import java.security.Principal;
+import java.util.Optional;
 import java.util.Random;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -14,9 +21,35 @@ public class HomeController {
 	@Resource
 	LogEntryRepository logEntryRepo;
 
+	@Autowired
+	UserRepository userRepo;
+
 	@GetMapping("/")
 	public String getIndex() {
 		return "index";
+	}
+
+	@GetMapping("/hello")
+	public String getHello() {
+		return "hello";
+	}
+
+	@RequestMapping("/demo")
+	public String demoPage(HttpServletRequest request, Model model, Principal principal, Authentication authentication) {
+		
+		// SEE: http://www.baeldung.com/get-user-in-spring-security
+		// ANY of these methods will work to retrieve the username
+		
+		// String username = request.getRemoteUser();
+		// String username = principal.getName();
+		String username = authentication.getName();
+		Optional<User> userOpt = userRepo.findByUsername(username);
+		if (!userOpt.isPresent()) {
+			throw new UsernameNotFoundException("User not found.");
+		}
+		model.addAttribute("user", userOpt.get());
+		
+		return "demo";
 	}
 
 	@GetMapping("/log")
---
+++ src/main/java/com/mybiblelog/ThymeleafHelperConfig.java
@@ -0,0 +1,61 @@
+package com.mybiblelog;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+public class ThymeleafHelperConfig {
+
+	@Autowired
+	UserRepository userRepo;
+	
+	// Enables Thymeleaf layouts (must be manually included since Spring Boot 2)
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
+	}
+	
+	// This class is for calling Java functionality from within a Thymeleaf template
+	// It could be used to test authentication/authorization,
+	// or to load data within a partial so that each controller
+	// doesn't have to provide the same contextual data to the model
+	@Bean("identity")
+	public ThymeIdentity identity() {
+		return new ThymeIdentity();
+	}
+	
+	public class ThymeIdentity {
+
+		public boolean loggedIn() {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			if (authentication == null) return false;
+			if (!authentication.isAuthenticated()) return false;
+			if (authentication instanceof AnonymousAuthenticationToken) return false;
+			return true;
+		}
+		
+		public boolean hasRole(String role) {
+			if (!this.loggedIn()) return false;
+			
+			Optional<User> userOpt = userRepo.findByUsername(this.username());
+			if (!userOpt.isPresent()) return false;
+			
+			User user = userOpt.get();
+			return Arrays.asList(user.getRoles()).contains(role);
+		}
+		
+		public String username() {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			return authentication.getName();
+		}
+	}
+}
