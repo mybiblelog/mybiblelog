e99e805ed6b81e1508299a76d9b45309b8bec4d7
Aaron <aaronsmith.xci@gmail.com>
2019-05-21 19:07:12 -0400

    Started frontend bible helper script

---
+++ src/main/resources/static/js/bible.js
@@ -0,0 +1,20 @@
+(() => {
+
+	const Bible = {};
+	
+	Bible.makeVerseId = (book, chapter, verse) => {
+		let verseId = 100000000 + book * 1000000 + chapter * 1000 + verse;
+		return verseId;
+	};
+
+	Bible.parseVerseId = verseId => {
+		verseId -= 100000000;
+		let book = Math.floor(verseId / 1000000);
+		verseId -= book * 1000000;
+		let chapter = Math.floor(verseId / 1000);
+		verseId -= chapter * 1000;
+		let verse = verseId;
+		return { book, chapter, verse };
+	};
+
+})(window.Bible = window.Bible || {});
