4c7fa16eb297f6a84a85fa037bddd4f84fdaa30f
Aaron <aaronsmith.xci@gmail.com>
2019-05-28 22:59:58 -0400

    Remove old code

--- src/main/java/com/mybiblelog/MyBibleLogApplication.java
+++ src/main/java/com/mybiblelog/MyBibleLogApplication.java
@@ -9,13 +9,6 @@ import com.mybiblelog.bible.BibleIndex;
 public class MyBibleLogApplication {
 
 	public static void main(String[] args) {
-		
-		BibleIndex bible = BibleIndex.getInstance();
-		System.out.println("Loading books:");
-		System.out.println(bible.getBooks());
-		System.out.println("Loading chapter verses:");
-		System.out.println(bible.getChapterVerses());
-		
 		SpringApplication.run(MyBibleLogApplication.class, args);
 	}
 
