44d3cdf10801e855e160aaaaed33abc88baceb9c
Aaron <aaronsmith.xci@gmail.com>
2019-06-01 23:08:32 -0400

    Added to LogEntryRestControllerTest

--- src/test/java/com/mybiblelog/logentry/LogEntryRestControllerTest.java
+++ src/test/java/com/mybiblelog/logentry/LogEntryRestControllerTest.java
@@ -1,11 +1,15 @@
 package com.mybiblelog.logentry;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
 import org.junit.Before;
@@ -22,7 +26,12 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mybiblelog.config.LoginService;
 import com.mybiblelog.user.User;
 import com.mybiblelog.user.UserRepository;
@@ -63,8 +72,20 @@ public class LogEntryRestControllerTest {
 		when(mockLogEntry.getId()).thenReturn(1L);
 	}
 	
-	// READ ALL
+	private String mapToJson(Object object) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(object);
+	}
 	
+	private <T> T mapFromJson(String json, Class<T> type)
+		throws JsonParseException, JsonMappingException, IOException {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, type);
+	}
+	
+	// READ ALL
+
 	@WithMockUser("test@example.com")
     @Test
     public void shouldGetAllLogEntriesWithStatus200() throws Exception {
@@ -73,11 +94,31 @@ public class LogEntryRestControllerTest {
         	.contentType(MediaType.APPLICATION_JSON))
         	.andExpect(status().isOk());
     }
+
+	@WithMockUser("test@example.com")
+    @Test
+    public void shouldReturnArrayOfLogEntries() throws Exception {
+		Collection<LogEntry> logEntries = new ArrayList<LogEntry>();
+		logEntries.add(new LogEntry());
+		logEntries.add(new LogEntry());
+		when(logEntryRepo.findAllByUserOrderByDateAsc(mockUser)).thenReturn(logEntries);
+		
+        MvcResult result = mvc
+    		.perform(get("/api/log-entries")
+    		.accept(MediaType.APPLICATION_JSON_VALUE))
+    		.andReturn();
+        
+        String content = result.getResponse().getContentAsString();
+        LogEntry[] logEntryList = this.mapFromJson(content, LogEntry[].class);
+        assertTrue(logEntryList.length > 0);
+    }
+	
 	
 	@Test
-	@Ignore("NOT IMPLEMENTED")
-	public void shouldReturn401IfNotAuthorizedToGetAll() {
-		//
+	public void shouldReturn302IfNotAuthorizedToGetAll() throws Exception {
+		mvc
+			.perform(get("/api/log-entries"))
+	    	.andExpect(status().is(302));
 	}
 	
 	// READ ONE
@@ -145,7 +186,7 @@ public class LogEntryRestControllerTest {
 	
 	@Test
 	@Ignore("NOT IMPLEMENTED")
-	public void shouldReturn401IfNotAuthorizedToCreate() {
+	public void shouldReturn302IfNotAuthorizedToCreate() {
 		//
 	}
 	
@@ -181,7 +222,7 @@ public class LogEntryRestControllerTest {
 
 	@Test
 	@Ignore("NOT IMPLEMENTED")
-	public void shouldReturn401IfNotAuthorizedToUpdate() {
+	public void shouldReturn302IfNotAuthorizedToUpdate() {
 		//
 	}
 	
@@ -203,7 +244,7 @@ public class LogEntryRestControllerTest {
 	
 	@Test
 	@Ignore("NOT IMPLEMENTED")
-	public void shouldReturn401IfNotAuthorizedToDelete() {
+	public void shouldReturn302IfNotAuthorizedToDelete() {
 		//
 	}
 }
