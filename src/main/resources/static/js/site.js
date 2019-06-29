155159a7e9a3a4bad871379f64cd835f643c85a7
Aaron <aaronsmith.xci@gmail.com>
2019-06-29 17:44:33 -0400

    Mobile nav slide in animation

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,10 +1,17 @@
 (() => {
   
-	const siteNav = document.querySelector('.site-nav');
-	const siteNavTrigger = document.querySelector('.site-nav-trigger');
+  const siteNav = document.querySelector('.site-nav');
+  const siteNavScreen = document.querySelector('.site-nav-screen');
+  const siteNavTrigger = document.querySelector('.site-nav-trigger');
   
 	siteNavTrigger.addEventListener('click', () => {
-	  siteNav.classList.toggle('active');
-	});
+    siteNav.classList.toggle('active');
+    siteNavScreen.classList.toggle('active');
+  });
+  
+	siteNavScreen.addEventListener('click', () => {
+    siteNav.classList.remove('active');
+    siteNavScreen.classList.remove('active');
+  });
   
 })();
