751ce6f329f25323f16373ce73dc2dc212bc8bca
Aaron <aaronsmith.xci@gmail.com>
2019-06-30 00:07:37 -0400

    Nav UX improvements

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,22 +1,25 @@
 (() => {
+
+  const breakpoint = 800;
+  let navOpen = false;
   
   const siteNav = document.querySelector('.site-nav');
   const siteNavScreen = document.querySelector('.site-nav-screen');
   const siteNavTrigger = document.querySelector('.site-nav-trigger');
   const siteNavTriggerIcon = document.querySelector('.site-nav-trigger-icon');
-  
-  let navOpen = false;
 
   const update = () => {
     if (navOpen) {
       siteNav.classList.add('active');
       siteNavScreen.classList.add('active');
       siteNavTriggerIcon.classList.add('active');
+      document.body.classList.add('modal-open');
     }
     else {
       siteNav.classList.remove('active');
       siteNavScreen.classList.remove('active');
       siteNavTriggerIcon.classList.remove('active');
+      document.body.classList.remove('modal-open');
     }
   };
 
@@ -28,6 +31,13 @@
 	siteNavScreen.addEventListener('click', () => {
     navOpen = false;
     update();
+  });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= breakpoint && navOpen) {
+      navOpen = false;
+      update();
+    }
   });
   
 })();
