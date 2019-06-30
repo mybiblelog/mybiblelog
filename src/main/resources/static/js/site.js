567f93f548bf3c0d7b18babfb735621b0b57f020
Aaron <aaronsmith.xci@gmail.com>
2019-06-29 23:59:39 -0400

    Better mobile nav

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -3,15 +3,31 @@
   const siteNav = document.querySelector('.site-nav');
   const siteNavScreen = document.querySelector('.site-nav-screen');
   const siteNavTrigger = document.querySelector('.site-nav-trigger');
+  const siteNavTriggerIcon = document.querySelector('.site-nav-trigger-icon');
   
+  let navOpen = false;
+
+  const update = () => {
+    if (navOpen) {
+      siteNav.classList.add('active');
+      siteNavScreen.classList.add('active');
+      siteNavTriggerIcon.classList.add('active');
+    }
+    else {
+      siteNav.classList.remove('active');
+      siteNavScreen.classList.remove('active');
+      siteNavTriggerIcon.classList.remove('active');
+    }
+  };
+
 	siteNavTrigger.addEventListener('click', () => {
-    siteNav.classList.toggle('active');
-    siteNavScreen.classList.toggle('active');
+    navOpen = !navOpen;
+    update();
   });
   
 	siteNavScreen.addEventListener('click', () => {
-    siteNav.classList.remove('active');
-    siteNavScreen.classList.remove('active');
+    navOpen = false;
+    update();
   });
   
 })();
